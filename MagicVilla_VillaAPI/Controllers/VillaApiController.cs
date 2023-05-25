c28e536e49ccf3eacb549090d3524bedf1045f8a
seanarch <zhuqingyun@outlook.com>
2023-05-25 15:36:04 -0600

    Added Controller and Villa class

---
+++ MagicVilla_VillaAPI/Controllers/VillaApiController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Name="Pool View"},
+                new Villa{Id=2, Name="Beach View"}
+            };
+        }
+    }
+}
+
