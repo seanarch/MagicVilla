8f2c4cf612603196f905fcec5ce4ec9f9e7035a0
seanarch <zhuqingyun@outlook.com>
2023-05-25 18:04:24 -0600

    Added verification of id

--- MagicVilla_VillaAPI/Controllers/VillaApiController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaApiController.cs
@@ -20,7 +20,17 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet("{id:int}")]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return Ok(VillaStore.villaList.FirstOrDefault(u=>u.Id==id));
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
     }
 }
