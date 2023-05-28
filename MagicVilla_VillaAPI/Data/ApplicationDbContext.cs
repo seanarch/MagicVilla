e2aa74433aeb2c703ba5da76ea70615761d6064e
seanarch <zhuqingyun@outlook.com>
2023-05-28 16:50:16 -0600

    Setup database and exclude settings file

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MagicVilla_VillaAPI.Models;
+
+namespace MagicVilla_VillaAPI.Data
+{
+	public class ApplicationDbContext
+	{
+		public DbSet<Villa> Villas { get; set; }
+	}
+}
+
--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -10,6 +10,10 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
 
+        public string Details { get; set; }
+
+        public double Rate { get; set; }
+
         public int Occupancy { get; set; }
 
         public int Sqft { get; set; }
